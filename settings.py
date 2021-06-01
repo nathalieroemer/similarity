babcae29ad1ac57b7de1a40a04de78ab44b01334
nathalieroemer <nathalie.roemer@yahoo.de>
2021-06-01 09:27:49 +0200

    update file and adjust experiment to 2 treatments

--- settings.py
+++ settings.py
@@ -43,7 +43,13 @@ SESSION_CONFIGS = [
         app_sequence=['tr7_1_intro_task', 'tr7_2_investment'],
         ## hier kann ich auch weitere apps hinzufügen, namen entsprechen ordner namen
     ),
-
+    dict(
+        name='tr2_1_intro_task',  ## name muss app name entsprechen
+        display_name='Supporters (2 treatments)',  ### display name kann gleich sein
+        num_demo_participants=50,
+        app_sequence=['tr2_1_intro_task', 'tr2_2_investment'],
+        ## hier kann ich auch weitere apps hinzufügen, namen entsprechen ordner namen
+    ),
     dict(
         name='tr4_1_intro_task',  ## name muss app name entsprechen
         display_name='Supporters (4 treatments)',  ### display name kann gleich sein
---
+++ tr2_2_investment/models.py
@@ -0,0 +1,78 @@
+from otree.api import (
+    models,
+    widgets,
+    BaseConstants,
+    BaseSubsession,
+    BaseGroup,
+    BasePlayer,
+    Currency as c,
+    currency_range,
+)
+
+
+author = 'Your name here'
+
+doc = """
+Your app description
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'tr2_decisions'
+    players_per_group = None
+    num_rounds = 10
+
+
+class Subsession(BaseSubsession):
+    pass
+
+class Group(BaseGroup):
+    pass
+
+class Player(BasePlayer):
+
+    word_1 = models.StringField()
+    word_2 = models.StringField()
+
+    value_1 = models.IntegerField()
+    value_2 = models.IntegerField()
+
+    photoid1 = models.StringField()
+    photoid2 = models.StringField()
+
+  #  photo_verbal_1 = models.LongStringField()
+ #   photo_verbal_2 = models.LongStringField()
+
+    female_1 = models.IntegerField()
+    female_2 = models.IntegerField()
+
+    x = models.IntegerField()
+    y = models.IntegerField()
+
+    obs_num1 = models.IntegerField()
+    obs_num2 = models.IntegerField()
+
+    right_choice = models.IntegerField()
+
+    list = models.LongStringField()
+    chose_1 = models.IntegerField()
+    chose_2 = models.IntegerField()
+
+    equal_value = models.IntegerField(
+        initial=0
+    )
+
+    roundcount = models.IntegerField()
+
+    gender = models.IntegerField()
+    english = models.IntegerField()
+    english_prof = models.IntegerField(
+        blank=True
+    )
+    colourb = models.IntegerField(
+        blank=True
+    )
+    stereotypes = models.IntegerField()
+    chance_bonus = models.IntegerField()
+    risk_pref = models.IntegerField()
+    comp = models.IntegerField()
